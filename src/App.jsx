e5d2fe4073b81210189b170c160061747dd57fb6
PankajMaurya <maurya2498@gmail.com>
2025-05-20 17:48:10 +0530

    Inline CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import {NetflixSeries} from "./component/NetflixSeries";  // --> works when we export while starting the component
+import { NetflixSeries } from "./component/NetflixSeries";  // --> works when we export while starting the component
 // import { NetflixSeries, Footer } from "./component/NetflixSeries";  // --> works when we export while starting the component
 // import NetflixSeries from "./component/NetflixSeries"; //--> works when export default NetflixSeries; written in last
+import "./component/Netflix.css"
 
 export const App = () => {
 
     return (
-        <>
-                < NetflixSeries />
-        </>
+        <section className="container">
+            <h1 className="card-heading">List of Best Netflix Series</h1>
+            < NetflixSeries />
+        </section>
     );
 
 };
--- src/component/SeriesCard.jsx
+++ src/component/SeriesCard.jsx
@@ -1,20 +1,30 @@
-export const SeriesCard = ({data}) => {
+export const SeriesCard = ({ data }) => {
     // console.log(props);
 
-    const { id, img_url, name, rating, description, genre, cast, watch_url } = data;
+    const { img_url, name, rating, description, genre, cast, watch_url } = data;
 
+    const btn_style = {
+        padding: "1.2rem 2.4rem",
+        border: "none",
+        fontSize: "1.6rem",
+        background: "var( --bnt-hover-bg-color)",
+        color: "var( --bg-color)"
+    };
     return (
-        <li key={id}>
+        <li className="card">
             <div>
                 <img src={img_url} alt={name} width="40%" height="50%" />
             </div>
-            <h2>Name: {name} </h2>
-            <h3>Rating: {rating} </h3>
-            <p>Description: {description}</p>
-            <p>Genre: {genre}</p>
-            <p>Cast: {cast}</p>
+            <div className="card-content" style={{ margin: "1.2rem 0" }}>
+                <h2>Name: {name} </h2>
+                <h3>Rating: {rating} </h3>
+                <p >Description: {description}</p>
+                <p>Genre: {genre}</p>
+                <p>Cast: {cast}</p>
 
-            <button><a href={watch_url} target="_blank">Watch Now</a></button>
+                <a href={watch_url} target="_blank"><button
+                    style={btn_style}>Watch Now</button></a>
+            </div>
         </li>
     );
 };
